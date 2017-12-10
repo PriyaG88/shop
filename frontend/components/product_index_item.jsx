1c5786399423d005f76bf8d6ab653b0e327f334e
PriyaG88 <priya.g71488@gmail.com>
2017-12-10 12:48:16 -0800

    Add Link to each product item

--- frontend/components/product_index_item.jsx
+++ frontend/components/product_index_item.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const ProductIndexItem = ( {product} ) => {
   return (
     <div className='product-grid-container'>
-      <div className='product-image-container'>
-        <img src={product.image_url}></img>
-      </div>
-      <div className='product-info-container'>
-        <div className='product-title'>
-          {product.title}
+      <Link to={`/products/${product.id}`}>
+        <div className='product-image-container'>
+          <img src={product.image_url}></img>
         </div>
-        <div className='product-price'>
-          ${product.price}
+        <div className='product-info-container'>
+          <div className='product-title'>
+            {product.title}
+          </div>
+          <div className='product-price'>
+            ${product.price}
+          </div>
         </div>
-      </div>
+      </Link>
     </div>
   );
 };
