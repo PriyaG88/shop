a4c02b8c849361abbdd65de532fbc1509de4791e
PriyaG88 <priya.g71488@gmail.com>
2017-12-15 11:51:59 -0800

    Add conditional to render alpha or numeric sizing in size chart component

--- frontend/components/SizeChart.jsx
+++ frontend/components/SizeChart.jsx
@@ -1,23 +1,58 @@
 import React from 'react';
 
 const SizeChart = (props) => {
+  if (props.category) {
+    return (
+      <div className="add-to-cart-container">
+        <form className="add-to-cart-form">
+          <label className="size-label">XS
+            <input name="sizeSelect" type="radio" value="XS" className="size-input"></input>
+          </label>
+          <label className="size-label">S
+            <input name="sizeSelect" type="radio" value="S" className="size-input"></input>
+          </label>
+          <label className="size-label">M
+            <input name="sizeSelect" type="radio" value="M" className="size-input"></input>
+          </label>
+          <label className="size-label">L
+            <input name="sizeSelect" type="radio" value="L" className="size-input"></input>
+          </label>
+          <label className="size-label">XL
+            <input name="sizeSelect" type="radio" value="XL" className="size-input"></input>
+          </label>
+        </form>
+      </div>
+    );
+  }
   return (
     <div className="add-to-cart-container">
       <form className="add-to-cart-form">
-        <label className="size-label">XS
-          <input name="sizeSelect" type="radio" value="XS" className="size-input"></input>
+        <label className="size-label">6
+          <input name="sizeSelect" type="radio" value="6" className="size-input"></input>
         </label>
-        <label className="size-label">S
-          <input name="sizeSelect" type="radio" value="S" className="size-input"></input>
+        <label className="size-label">6.5
+          <input name="sizeSelect" type="radio" value="6.5" className="size-input"></input>
         </label>
-        <label className="size-label">M
-          <input name="sizeSelect" type="radio" value="M" className="size-input"></input>
+        <label className="size-label">7
+          <input name="sizeSelect" type="radio" value="7" className="size-input"></input>
         </label>
-        <label className="size-label">L
-          <input name="sizeSelect" type="radio" value="L" className="size-input"></input>
+        <label className="size-label">7.5
+          <input name="sizeSelect" type="radio" value="7.5" className="size-input"></input>
         </label>
-        <label className="size-label">XL
-          <input name="sizeSelect" type="radio" value="XL" className="size-input"></input>
+        <label className="size-label">8
+          <input name="sizeSelect" type="radio" value="8" className="size-input"></input>
+        </label>
+        <label className="size-label">8.5
+          <input name="sizeSelect" type="radio" value="8.5" className="size-input"></input>
+        </label>
+        <label className="size-label">9
+          <input name="sizeSelect" type="radio" value="9" className="size-input"></input>
+        </label>
+        <label className="size-label">9.5
+          <input name="sizeSelect" type="radio" value="9.5" className="size-input"></input>
+        </label>
+        <label className="size-label">10
+          <input name="sizeSelect" type="radio" value="10" className="size-input"></input>
         </label>
       </form>
     </div>
