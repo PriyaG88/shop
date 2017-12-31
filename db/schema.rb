96662fc40c2674df6c5108aba86962fec6918559
PriyaG88 <priya.g71488@gmail.com>
2017-12-30 18:42:53 -0800

    Create cart items table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171231023222) do
+ActiveRecord::Schema.define(version: 20171231023836) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cart_items", force: :cascade do |t|
+    t.integer "product_id", null: false
+    t.integer "cart_id", null: false
+    t.integer "quantity", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["cart_id"], name: "index_cart_items_on_cart_id"
+  end
 
   create_table "carts", force: :cascade do |t|
     t.integer "user_id", null: false
