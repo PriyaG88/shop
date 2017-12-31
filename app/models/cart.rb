03ce15d015aab6f7666d49429598b6579e814206
PriyaG88 <priya.g71488@gmail.com>
2017-12-30 18:52:18 -0800

    Define associations and validations for cart and cart items

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,2 +1,4 @@
 class Cart < ApplicationRecord
+  validates :user_id, presence: true
+  has_many :cart_items
 end
--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,2 +1,4 @@
 class CartItem < ApplicationRecord
+  validates :product_id, :cart_id, :quantity, presence: true
+  belongs_to :cart
 end
