987183216e07596371ca47faa4c65837a3f40859
PriyaG88 <priya.g71488@gmail.com>
2018-01-02 15:27:36 -0800

    Add user id to cart items

--- test/models/cart_item_test.rb
+++ test/models/cart_item_test.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: cart_items
+#
+#  id         :integer          not null, primary key
+#  product_id :integer          not null
+#  quantity   :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  size       :string
+#  user_id    :integer
+#
+
 require 'test_helper'
 
 class CartItemTest < ActiveSupport::TestCase
