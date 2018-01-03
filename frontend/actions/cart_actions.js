8dabf556862a6fc25dec9deebcaa1968ca172f0a
PriyaG88 <priya.g71488@gmail.com>
2018-01-02 16:40:14 -0800

    Add cart item errors reducer

--- frontend/actions/cart_actions.js
+++
@@ -1,6 +0,0 @@
-import * as CartAPIUtil from '../util/cart_api_util';
-
-export const addToCart = id => dispatch => (
-  CartAPIUtil.addToCart(id).then(item => dispatch(receiveCartItem(item)),
-  err => ( dispatch(receiveCartItemErrors(err.responseJSON))))
-);
---
+++ frontend/reducers/cart_item_errors_reducer.js
@@ -0,0 +1,25 @@
+import merge from 'lodash/merge';
+import {
+  RECEIVE_CART_ITEM_ERRORS,
+  RECEIVE_CART_ITEMS_ERRORS,
+  RECEIVE_CART_ITEM,
+  RECEIVE_CART_ITEMS
+ }
+  from '../actions/cart_item_actions';
+
+const CartItemErrorsReducer = (state, action) => {
+  Object.freeze(state);
+
+  switch(action.type) {
+    case RECEIVE_CART_ITEM_ERRORS:
+      return merge({}, action.payload);
+    case RECEIVE_CART_ITEMS_ERRORS:
+      return merge({}, action.payload);
+    case RECEIVE_CART_ITEM:
+      return [];
+    case RECEIVE_CART_ITEMS:
+      return [];
+    default:
+      return state;
+  }
+};
