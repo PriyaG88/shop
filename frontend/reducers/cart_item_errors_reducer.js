3ee75806bec9c8bf3d256957c74687fbceee122d
PriyaG88 <priya.g71488@gmail.com>
2018-01-02 16:42:13 -0800

    Add cart item errors reducer to errors reducer

--- frontend/reducers/cart_item_errors_reducer.js
+++ frontend/reducers/cart_item_errors_reducer.js
@@ -23,3 +23,5 @@ const CartItemErrorsReducer = (state, action) => {
       return state;
   }
 };
+
+export default CartItemErrorsReducer;
--- frontend/reducers/errors_reducer.js
+++ frontend/reducers/errors_reducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import SessionErrorsReducer from './session_errors_reducer';
 import ProductErrorsReducer from './product_errors_reducer';
 import CategoryErrorsReducer from './category_errors_reducer';
+import CartItemErrorsReducer from './cart_item_errors_reducer';
 
 const ErrorsReducer = combineReducers({
   session: SessionErrorsReducer,
   category: CategoryErrorsReducer,
-  product: ProductErrorsReducer
+  product: ProductErrorsReducer,
+  cart: CartItemErrorsReducer,
 });
 
 export default ErrorsReducer;
