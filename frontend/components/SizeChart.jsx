6def2ef3455aa50744e9a1fa5a4f767598b50f2c
PriyaG88 <priya.g71488@gmail.com>
2018-01-03 13:25:02 -0800

    Add event handlers for numerically sized product

--- frontend/components/SizeChart.jsx
+++ frontend/components/SizeChart.jsx
@@ -5,7 +5,9 @@ class SizeChart extends Component {
     super(props);
     this.state = {
       product: '',
-      size: ''
+      product_id: this.props.productId,
+      quantity: 1,
+      size: '',
     };
   }
 
@@ -16,7 +18,18 @@ class SizeChart extends Component {
 
   handleChange(e) {
     this.setState({size: e.currentTarget.value});
+  }
+
+  handleClick(e) {
+    e.preventDefault();
     console.log(this.state);
+    if (this.state.quantity > 0) {
+      this.props.addToCart({
+        product_id: this.state.product_id,
+        quantity: this.state.quantity,
+        size: this.state.size
+      });
+    }
   }
 
   render() {
@@ -25,14 +38,17 @@ class SizeChart extends Component {
       return (
         <div className="add-to-cart-container">
           <span className="size-select-direction">Choose Size</span>
+          <form className="add-to-cart-form">
             <select onChange={this.handleChange.bind(this)} name="alpha-size-options" value={this.state.size}>
+              <option selected value="Select a Size">Select a Size</option>
               <option value="X-Small">X-Small</option>
               <option value="Small">Small</option>
               <option value="Medium">Medium</option>
               <option value="Large">Large</option>
               <option value="X-Large">X-Large</option>
             </select>
-            <button className="add-to-cart-btn">Add to Cart</button>
+            <button onClick={this.handleClick.bind(this)} className="add-to-cart-btn">Add to Cart</button>
+          </form>
         </div>
       );
     }
@@ -41,6 +57,7 @@ class SizeChart extends Component {
         <span className="size-select-direction">Choose Size</span>
         <form className="add-to-cart-form">
           <select onChange={this.handleChange.bind(this)} name="alpha-size-options" value={this.state.size}>
+            <option value="Select a Size">Select a Size</option>
             <option value="6">6</option>
             <option value="6.5">6.5</option>
             <option value="7">7</option>
@@ -51,7 +68,7 @@ class SizeChart extends Component {
             <option value="9.5">9.5</option>
             <option value="10">10</option>
           </select>
-          <button className="add-to-cart-btn">Add to Cart</button>
+          <button onClick={this.handleClick.bind(this)} className="add-to-cart-btn">Add to Cart</button>
         </form>
       </div>
     );
--- frontend/components/SizeChartContainer.js
+++ frontend/components/SizeChartContainer.js
@@ -1,6 +1,7 @@
 import { connect } from 'react-redux';
 import {withRouter} from 'react-router-dom';
 import {fetchProduct} from '../actions/product_actions';
+import { addToCart } from '../actions/cart_item_actions';
 import {allProduct} from '../reducers/selector';
 import SizeChart from './SizeChart';
 
@@ -10,7 +11,8 @@ const mapStateToProps = (state, ownProps) => ({
 });
 
 const mapDispatchToProps = dispatch => ({
-  fetchProduct: id => dispatch(fetchProduct(id))
+  fetchProduct: id => dispatch(fetchProduct(id)),
+  addToCart: item => dispatch(addToCart(item))
 });
 
 export default withRouter(
