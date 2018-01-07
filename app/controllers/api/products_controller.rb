b17485fd8325e828b46a09499efda4f221613cf2
PriyaG88 <priya.g71488@gmail.com>
2018-01-06 19:04:32 -0800

    Remove whitelisting since all products have been seeded

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -8,10 +8,4 @@ class Api::ProductsController < ApplicationController
     @product = Product.find(params[:id])
   end
 
-  private
-
-  def product_params
-    params.require(:product).permit(:category_id, :image_url, :big_image_url,
-    :title, :price, :description)
-  end
 end
