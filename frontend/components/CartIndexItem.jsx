8547e7d5071bfac6cd52749dee7bb3c72a50c470
PriyaG88 <priya.g71488@gmail.com>
2018-01-25 23:26:37 -0800

    Refactor functional component to class based component

--- frontend/components/CartIndexItem.jsx
+++ frontend/components/CartIndexItem.jsx
@@ -1,29 +1,42 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-const CartIndexItem = ({ item }) => {
-  const { image_url, id, title } = item.product;
+class CartIndexItem extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-  return (
-    <div>
-      <Link to={`/products/${id}`}><img src={image_url} className="cart-item-img" /></Link>
-      <div className="cart-item-details">
-        <span className="cart-item-title">
-          <Link to={`/products/${id}`} className="cart-item-link">{title}</Link>
-        </span>
-        <div>
-          Size: <span>{item.size}</span>
-        </div>
-        <div>
-          Qty: <span>{item.quantity} @ ${item.product.price}</span>
-        </div>
-        <div className="cart-item-actions">
-          <input className="cart-item-btn" type="button" value="Remove"></input>
-          <input className="cart-item-btn" type="button" value="Edit"></input>
+  handleRemove = (e) => {
+    e.preventDefault();
+    this.props.removeFromCart(this.props.item.id);
+  }
+
+  render() {
+    const { image_url, id, title, price } = this.props.item.product;
+    const { size, quantity } = this.props.item;
+
+    return (
+      <div>
+        <Link to={`/products/${id}`}><img src={image_url} className="cart-item-img" /></Link>
+        <div className="cart-item-details">
+          <span className="cart-item-title">
+            <Link to={`/products/${id}`} className="cart-item-link">{title}</Link>
+          </span>
+          <div>
+            Size: <span>{size}</span>
+          </div>
+          <div>
+            Qty: <span>{quantity} @ ${price}</span>
+          </div>
+          <div className="cart-item-actions">
+            <input onClick={this.handleRemove} className="cart-item-btn" type="button" value="Remove" />
+            <input className="cart-item-btn" type="button" value="Edit" />
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+
+}
 
 export default CartIndexItem;
