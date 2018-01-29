0124e8d3953f9ada3bb2c9309159805d59bc1ca1
PriyaG88 <priya.g71488@gmail.com>
2018-01-28 17:59:27 -0800

    Add styling to header nav and cart items

--- app/controllers/api/cart_items_controller.rb
+++ app/controllers/api/cart_items_controller.rb
@@ -15,7 +15,7 @@ class Api::CartItemsController < ApplicationController
 
   def destroy
     @cart_item = CartItem.find(params[:id])
-    @cart_item.destroy
+    @cart_item.destroy!
   end
 
   private
