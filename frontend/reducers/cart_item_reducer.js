35c4ab9692a3b401b7741e655016bbe3a0fb4c6d
PriyaG88 <priya.g71488@gmail.com>
2018-02-02 22:03:20 -0800

    Add update cart item case

--- frontend/reducers/cart_item_reducer.js
+++ frontend/reducers/cart_item_reducer.js
@@ -1,17 +1,25 @@
 import merge from 'lodash/merge';
-import { RECEIVE_CART_ITEM, RECEIVE_CART_ITEMS, REMOVE_CART_ITEM }
-  from '../actions/cart_item_actions';
+import {
+  RECEIVE_CART_ITEM,
+  RECEIVE_CART_ITEMS,
+  UPDATE_CART_ITEM,
+  REMOVE_CART_ITEM
+} from '../actions/cart_item_actions';
 
 const CartItemReducer = (state = {}, action) => {
   Object.freeze(state);
-
+  let newState;
+  
   switch(action.type) {
     case RECEIVE_CART_ITEMS:
       return merge({}, action.payload);
     case RECEIVE_CART_ITEM:
       return merge({}, state, action.payload);
+    case UPDATE_CART_ITEM:
+      newState = merge({}, state, { [action.payload.id]: action.payload });
+      return newState;
     case REMOVE_CART_ITEM:
-      let newState = merge({}, state);
+      newState = merge({}, state);
       delete newState[action.payload];
       return newState;
     default:
