845d746cf4e41000b83276c115ae9fab4c3cfb6d
PriyaG88 <priya.g71488@gmail.com>
2018-02-02 22:00:13 -0800

    Add update action to Cart Items Controller

--- app/controllers/api/cart_items_controller.rb
+++ app/controllers/api/cart_items_controller.rb
@@ -13,6 +13,16 @@ class Api::CartItemsController < ApplicationController
     end
   end
 
+  def update
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.update_attributes(cart_item_params)
+    if @cart_item.save
+      render :show
+    else
+      render json: @cart_item.errors.full_messages, status: 422
+    end
+  end
+
   def destroy
     @cart_item = CartItem.find(params[:id])
     @cart_item.destroy!
